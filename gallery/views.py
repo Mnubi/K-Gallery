719d28fc765f2d4030b2a24619b19dd7c1cce56f
mnubi <rodger.kamau@student.moringaschool.com>
2021-11-30 10:16:20 +0300

    created headline for the application

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.shortcuts import render
 
 # Create your views here.
 
 
 def welcome(request):
     return render(request, 'welcome.html')
+
+def index(request):
+    
+    return render(request, 'index.html')
